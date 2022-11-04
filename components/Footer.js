31e55083175f3715446901afdef026cb0fe35fcc
thecarrier <splandz@outlook.com>
2022-11-04 14:53:38 +0100

    footer date update

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const siteFooter = () => {
     return ( 
         <footer>
-            <p><Link href="/">PhiloPractice 2021</Link>, <Link href="mailto:djordje.matejic@gmail.com">djordjem</Link></p>
+            <p><Link href="/">PhiloPractice 2022</Link>, <Link href="mailto:djordje.matejic@gmail.com">djordjem</Link></p>
         </footer>
      );
 }
